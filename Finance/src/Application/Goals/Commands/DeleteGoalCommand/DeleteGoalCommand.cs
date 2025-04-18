cbe43cdccfc01aa99bdef465bc4b6031cb9e0e31
Valery Noskov <rahdah06@gmail.com>
2025-04-18 14:51:21 +0300

    feat: added delete command with tests.

---
+++ Finance/src/Application/Goals/Commands/DeleteGoalCommand/DeleteGoalCommand.cs
@@ -0,0 +1,22 @@
+﻿using Finance.Application.Common.Interfaces;
+
+namespace Finance.Application.Goals.Commands.DeleteGoalCommand;
+
+public record DeleteGoalCommand(int Id) : IRequest;
+
+public class DeleteGoalCommandHandler(IApplicationDbContext context, IUser user) : IRequestHandler<DeleteGoalCommand>
+{
+    public async Task Handle(DeleteGoalCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await context.Goals
+            .Include(x => x.Board)
+            .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+        
+        Guard.Against.NotFound(request.Id, entity);
+        if (user?.Id is null || !entity.Board!.UserIds!.Contains(user.Id))
+            throw new UnauthorizedAccessException();
+
+        context.Goals.Remove(entity);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ Finance/tests/Application.FunctionalTests/Goals/Command/DeleteGoalTests.cs
@@ -0,0 +1,46 @@
+﻿using Finance.Application.Boards.Commands.CreateBoardCommand;
+using Finance.Application.Goals.Commands.CreateGoal;
+using Finance.Application.Goals.Commands.DeleteGoalCommand;
+using Finance.Domain.Entities;
+
+namespace Finance.Application.FunctionalTests.Goals.Command;
+
+public class DeleteGoalTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldRequireCurrentUser()
+    {
+        var userId = await Testing.RunAsUserAsync("User1", "Password1!", []);
+        var boardId = await Testing.SendAsync(new CreateBoardCommand("Test Board"));
+        var goalId = await Testing.SendAsync(new CreateGoalCommand("Test Goal", 0, DateOnly.MinValue, boardId, null));
+        var anotherUserId = await Testing.RunAsUserAsync("User2", "Password2!", []);
+        
+        await FluentActions
+            .Invoking(() => Testing.SendAsync(new DeleteGoalCommand(goalId)))
+            .Should().ThrowAsync<UnauthorizedAccessException>();
+    }
+
+    [Test]
+    public async Task ShouldRequireGoalExists()
+    {
+        var userId = await Testing.RunAsUserAsync("User1", "Password1!", []);
+        var boardId = await Testing.SendAsync(new CreateBoardCommand("Test Board"));
+        
+        await FluentActions
+            .Invoking(() => Testing.SendAsync(new DeleteGoalCommand(0)))
+            .Should().ThrowAsync<NotFoundException>();
+    }
+
+    [Test]
+    public async Task ShouldDeleteGoal()
+    {
+        var userId = await Testing.RunAsUserAsync("User1", "Password1!", []);
+        var boardId = await Testing.SendAsync(new CreateBoardCommand("Test Board"));
+        var goalId = await Testing.SendAsync(new CreateGoalCommand("Test Goal", 0, DateOnly.MinValue, boardId, null));
+        
+        await Testing.SendAsync(new DeleteGoalCommand(goalId));
+
+        var goal = await Testing.FindAsync<Goal>(goalId);
+        goal.Should().BeNull();
+    }
+}
