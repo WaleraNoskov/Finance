b1141c4c7ddd7dab1ece4bcffc8e834339036b2c
Valery Noskov <rahdah06@gmail.com>
2025-05-21 09:36:40 +0300

    feat: added Boards api.

---
+++ Finance/src/Web/Endpoints/Boards.cs
@@ -0,0 +1,26 @@
+﻿using Finance.Application.Boards.Commands.CreateBoardCommand;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Finance.Web.Endpoints;
+
+public class Boards : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .RequireAuthorization()
+            .MapPost(Create);
+        // .MapGet(GetTodoItemsWithPagination)
+        // .MapPost(CreateTodoItem)
+        // .MapPut(UpdateTodoItem, "{id}")
+        // .MapPut(UpdateTodoItemDetail, "UpdateDetail/{id}")
+        // .MapDelete(DeleteTodoItem, "{id}");
+    }
+
+    public async Task<Created<int>> Create(ISender sender, CreateBoardCommand command)
+    {
+        var id = await sender.Send(command);
+        
+        return TypedResults.Created($"/{nameof(Boards)}/{id}", id);
+    }
+}
